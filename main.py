4e2cc27e24dfbfa585aeb4c6ca0bf1a80350c053
Brandon <58617118+BrandonC891@users.noreply.github.com>
2021-12-06 16:29:18 -0800

    update json parser

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
-from lxml import html 
-import requests 
-import json
-
-auth_url     = "https://api.openpeoplesearch.com/api/v1/User/authenticate"
-auth_data    = '{ "username" : "brandon.cannon88@gmail.com", "password" : "CnG2wDfE3789!"}'
-auth_headers = { "Content-Type" : "application/json", "accept" : "*/*" }
-
-search_url = "https://api.openpeoplesearch.com/api/v1/Consumer/PhoneSearch"
-search_data =   '{ "phoneNumber" : "4259238226" }'
-search_headers = { "accept" : "text/plain", "Authorization" : "", "Content-Type" : "application/json" }
-
-auth_response = None
-
-def http_error(e): 
-
-    codes = { 
-        400 : "Bad request", 
-        401 : "Unauthorized", 
-        402 : "Payment Required", 
-        500 : "Internal Server Error", 
-        503 : "Temporarily Unavailable", 
-        200 : "Valid request",
-    }
-
-    return codes.get(e, "Null Error")
-
-results_terms = {"firstName", "lastName", "address", "city", "state", "zip" }
-response_terms = {"First Name: ", "Last Name: ", "Address: ", "City: ", "State: ", "Zip Code: "}
+#from lxml import html 
+#import requests 
 
 
-try: 
+from plugins.json_parser import jsonParser as jp 
+
+output = jp('output.json') 
+
+#auth_url     = "https://api.openpeoplesearch.com/api/v1/User/authenticate"
+#auth_data    = '{ "username" : "brandon.cannon88@gmail.com", "password" : "CnG2wDfE3789!"}'
+#auth_headers = { "Content-Type" : "application/json", "accept" : "*/*" }
+ 
+#search_url = "https://api.openpeoplesearch.com/api/v1/Consumer/PhoneSearch"
+#search_data =   '{ "phoneNumber" : "4259238226" }'
+#search_headers = { "accept" : "text/plain", "Authorization" : "", "Content-Type" : "application/json" }
+
+#auth_response = None
+
+#def http_error(e): 
+
+#    codes = { 
+#        400 : "Bad request", 
+#        401 : "Unauthorized", 
+#        402 : "Payment Required", 
+#        500 : "Internal Server Error", 
+#        503 : "Temporarily Unavailable", 
+#        200 : "Valid request",
+#    }
+
+#    return codes.get(e, "Null Error")
+
+#results_terms = {"firstName", "lastName", "address", "city", "state", "zip" }
+#response_terms = {"First Name: ", "Last Name: ", "Address: ", "City: ", "State: ", "Zip Code: "}
+
+
+#try: 
     
-    auth_response = requests.post(auth_url, headers = auth_headers, data = auth_data)
+#    auth_response = requests.post(auth_url, headers = auth_headers, data = auth_data)
 
-    if auth_response.status_code != 200: 
-        raise Exception(http_error(auth_response.status_code))
+#    if auth_response.status_code != 200: 
+#        raise Exception(http_error(auth_response.status_code))
 
-    auth_token = auth_response.json()["token"]
+#    auth_token = auth_response.json()["token"]
 
-    search_headers["Authorization"] = ("Bearer {}").format(auth_token) 
+#    search_headers["Authorization"] = ("Bearer {}").format(auth_token) 
 
-    search_response = requests.post(search_url, headers= search_headers, data = search_data)
+#   search_response = requests.post(search_url, headers= search_headers, data = search_data)
 
-    if search_response.status_code != 200: 
-        raise Exception(http_error(search_response.status_code))
+#   if search_response.status_code != 200: 
+#        raise Exception(http_error(search_response.status_code))
 
-    filename = (search_response.json()["searchId"] + ".json") 
+#    filename = (search_response.json()["searchId"] + ".json") 
     
-    with open(filename, 'w') as output_file: 
-        json.dump(search_response.json(), output_file)
+#    with open(filename, 'w') as output_file: 
+#        json.dump(search_response.json(), output_file)
 
 
         
@@ -56,8 +60,8 @@ try:
 
     # print("Search Cost ($): {}\n Search Date: {}\n First Name: {}\nLast Name: {}\nAddress: {}\nCity: ()\nState: {}\nZip Code: {}").format(p_info[0], p_info[1], p_info[2], p_info[3], p_info[4], p_info[5], p_info[6], p_info[7])
 
-except Exception as e: 
-    print(("Error: {}").format(e)) 
+#except Exception as e: 
+#    print(("Error: {}").format(e)) 
 
 
 
--- plugins/json_parser.py
+++ plugins/json_parser.py
@@ -1,72 +1,69 @@
-import json 
 import os.path
+import json 
 
 class jsonParser: 
 
-    def __init__(self): 
+    def __init__(self, filename): 
         self.p = __file__
-        self.output = object
-        self.searchId = int
-        self.results = list
+        self.filename = filename 
+        self.output = object 
+        self.searchId = ''
+        self.results = dict
         self.input_terms = {
                 "firstName" : '', 
                 "lastName" : '', 
                 "address" : '', 
                 "city" : '', 
                 "state" : '', 
-                "zip" : ''
-        }
+                "zip" : '',
+                "email": '', 
+                "phone": '', 
+        },
         self.output_terms = {}
+        self.loadFile() 
 
-    def checkResults(self): 
-        if len(self.results) > 0: 
-            return True 
-        else:
-            return False 
-    
     def loadFile(self): 
-        try: 
-
-            self.p = open(os.path.join(os.path.split(os.path.dirname(__file__))[0], 'output/output.json'), 'r') 
         
+        try: 
+            self.p = open(os.path.join(os.path.split(os.path.dirname(__file__))[0], 'output/' + self.filename), 'r') 
         except OSError as e: 
-
             print("Error: {}".format(e)) 
 
         self.output = json.load(self.p) 
-
         self.searchId = self.output['searchId'] 
         self.results = self.output['results'] 
 
-        print("\nSearch ID".ljust(10), ':', self.searchId.ljust(20))
+        print("\nSearch ID ".ljust(10), ':', self.searchId.ljust(20))
         
-        if self.checkResults(): 
-            print("Results Found.") 
+        if len(self.results) > 0: 
+            self.parseInput() 
+            self.printOutput() 
         else: 
             print("No Results Found.") 
-        
         return 
 
     def parseInput(self): 
         delim = 'N'
 
         for key in self.input_terms: 
-            
-            res_pos = [i for i, e in enumerate(key+delim) if e.isupper()]
-            res_list = [key[res_pos[j]:res_pos[j+1]]
-                for j in range(len(res_pos)-1)]
+           for value in key:  
+                res_pos = [i for i, e in enumerate(value+delim) if e.isupper()]
+                res_list = [value[res_pos[j]:res_pos[j+1]]
+                    for j in range(len(res_pos)-1)]
 
-            if len(res_list) > 0: 
-                self.output_terms[key.split(res_list[0])[0].capitalize() + ' ' + res_list[0]] = self.input_terms[key]
-            else:
-                self.output_terms[key.capitalize()] = self.input_terms[key]
-
-        return 
+                if len(res_list) > 0: 
+                    self.output_terms[value.split(res_list[0])[0].capitalize() + ' ' + res_list[0]] = self.results[0][value]  
+                else:
+                    self.output_terms[value.capitalize()] = self.results[0][value] 
+        return  
 
     def printOutput(self): 
+        print() 
         for terms in self.output_terms: 
             print(terms.ljust(10), ':', self.output_terms[terms].ljust(20))
         print() 
+
+
 
 
 

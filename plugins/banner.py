596f398a848dec70ee25c169cffc84c547acbd63
Brandon <58617118+BrandonC891@users.noreply.github.com>
2021-12-08 16:14:24 -0800

    update banner

---
+++ plugins/banner.py
@@ -0,0 +1,13 @@
+class banner: 
+    def __init__(self): 
+        self.width = 100
+    
+    def display(self): 
+        print("___  ___                 ___  ______ _____".center(self.width))
+        print(" |  \/  |                / _ \ | ___ \_   _|".center(self.width))
+        print("| .  . | __ _ ___ ___  / /_\ \| |_/ / | |".center(self.width))
+        print("| |\/| |/ _` / __/ __| |  _  ||  __/  | |".center(self.width))
+        print("| |  | | (_| \__ \__ \ | | | || |    _| |_".center(self.width)) 
+        print("\_|  |_/\__,_|___/___/ \_| |_/\_|    \___/".center(self.width))
+        print("-*~-~*- A practice environment -*~-~*-".center(self.width))
+        print("~*-*-~ API implementation ~*-*-~".center(self.width))

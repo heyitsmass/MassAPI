4d27e751d9b7a2de5a84c799843d48f3d3f7f618
Brandon <58617118+heyitsmass@users.noreply.github.com>
2021-12-22 02:15:01 -0800

    redesign main.py

--- main.py
+++ main.py
@@ -1,98 +1,19 @@
-import json
-import os
+from plugins.menu import Menu 
+import curses
+from curses import panel
 
-import requests
-from lxml import html
+from plugins.userAuth import OPS_Auth
+from plugins.resources.banners import sub_banners
 
-from plugins.colors import Colors as color
-from plugins.json_parser import jsonParser as jp
-from plugins.menu import Menu
+class main(object): 
+    def __init__(self, stdscreen): 
+        self.screen = stdscreen 
+        curses.curs_set(0)
 
+        main_menu_items = [('Open People Search', OPS_Auth(self.screen).display)]
+        main_menu = Menu(main_menu_items, self.screen, sub_banners['main menu'])
 
-def http_error(e): 
-
-    codes = { 
-        400 : "Bad request", 
-        401 : "Unauthorized", 
-        402 : "Payment Required", 
-        500 : "Internal Server Error", 
-        503 : "Temporarily Unavailable", 
-        200 : "Valid request",
-    }
-
-    return codes.get(e, "Null Error")
-
-def main():
-
-    os.system('clear')  
-
-    auth_url = "https://api.openpeoplesearch.com/api/v1/User/authenticate"
-    auth_data = { "username" : "", "password" : ""}
-    auth_headers = { "Content-Type" : "application/json", "accept" : "*/*" }
-
-    search_url = "https://api.openpeoplesearch.com/api/v1/Consumer/PhoneSearch"
-    search_data =   '{ "phoneNumber" : "" }'
-    search_headers = { "accept" : "text/plain", "Authorization" : "", "Content-Type" : "application/json" }
-
-    auth_response = None 
-
-    Menu.display(auth_data) 
-
-    try: 
-        auth_response = requests.post(auth_url, headers=auth_headers, data=json.dumps(auth_data)) 
-
-        if auth_response.status_code != 200: 
-            raise Exception(http_error(auth_response.status_code)) 
-
-        print(f"{color.WHT}[{color.GRN}✓{color.WHT}] User sucessfully authenticated.{color.RST}")  
-        
-    except Exception as e: 
-        print(f"{color.WHT}[{color.RED}X{color.WHT}] Error: {color.RED}" + str(e) + color.RST)
-
-    auth_data["username"] = '-' 
-    auth_data["password"] = '-' 
-
-    print(f"{color.WHT}[{color.GRN}✓{color.WHT}] User information redacted.{color.RST}")
-
-    return 
-
-def main_two():
-
-    Menu.main_menu()
-
-
-    return 
+        main_menu.display()
 
 if __name__ == "__main__": 
-    #main() 
-    main_two() 
-
-#try: 
-
-#   search_headers["Authorization"] = ("Bearer {}").format(auth_response.json()["token"]) 
-
-#   search_response = requests.post(search_url, headers= search_headers, data = search_data)
-
-#   if search_response.status_code != 200: 
-#        raise Exception(http_error(search_response.status_code))
-
-#    filename = (search_response.json()["searchId"] + ".json") 
-    
-#    with open(filename, 'w') as output_file: 
-#        json.dump(search_response.json(), output_file)
-
-
-        
-    # p_info[0] = search_response.json()["lastName"] 
-
-    # print("Search Cost ($): {}\n Search Date: {}\n First Name: {}\nLast Name: {}\nAddress: {}\nCity: ()\nState: {}\nZip Code: {}").format(p_info[0], p_info[1], p_info[2], p_info[3], p_info[4], p_info[5], p_info[6], p_info[7])
-
-#except Exception as e: 
-#    print(("Error: {}").format(e)) 
-
-
-
-
-
-
-
+    curses.wrapper(main)

478bb6ef61a4f0f63b858fba206f3da75727bcb2
Brandon <58617118+heyitsmass@users.noreply.github.com>
2022-01-16 23:32:31 -0800

    updates

--- WhoisApi/test.py
+++ WhoisApi/test.py
@@ -1,5 +1,40 @@
-import json
-from mass_json import load
+filename = "information_sample.json" 
+data = open(filename, 'r').read() 
+
+json = {} 
+
+buffer = '' 
+
+i = 0 
+while i < len(data):
+    if data[i] == ',': 
+        break 
+    buffer += data[i] 
+    i+=1 
+
+buffer = buffer.split(': ', 1)
+
+for i in range(len(buffer)):
+    buffer[i] = str(buffer[i].strip('"'), )
+
+json[buffer[0]] = buffer[1]   
+
+print(json) 
+
+
+buffer = '' 
+
+hello = "hello"
+
+print(hello) 
+
+
+
+class J: 
+    def __init__(self): 
+        pass 
+
+    
 
 
 
